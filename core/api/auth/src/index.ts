874ed96396922db9cc4885a09e6497c371298bb9
yzia2000 <e0381439@u.nus.edu>
2021-01-03 00:48:25 +0530

    Working postgres and node

--- core/api/auth/src/index.ts
+++ core/api/auth/src/index.ts
@@ -1,13 +1,42 @@
-import express, { Application, Response } from 'express';
+import express, { Application } from 'express';
+import cors from 'cors';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    host: 'userdb',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'testdata'
+});
 
 const app: Application = express();
 
-const PORT: string = process.env.PORT || "5000";
+app.use(cors());
 
-app.get('/', (_req, res: Response) => {
+app.get('/', (_req, res) => {
     res.send("Hello");
 });
 
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let query: string = `SELECT * FROM students WHERE email = '${email}'`;
+    pool.query(query, (error, results) => {
+        if (error) {
+            console.error(error);
+        } else {
+            res.status(200).json(results.rows);
+        }
+    });
+});
+
+app.get('/signup', (_req, res) => {
+    res.send("signed up");
+});
+
+const PORT: string = process.env.PORT || "5000";
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 });
