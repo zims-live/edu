1325d067c0ece7599eab929e2ff51436de6ec3ec
yzia2000 <e0381439@u.nus.edu>
2021-01-18 15:33:53 +0530

    Parameterized sql queries

--- core/api/auth/src/index.ts
+++ core/api/auth/src/index.ts
@@ -1,8 +1,8 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import { Pool } from 'pg';
-import { dbConfig } from './config';
-import bodyParser from 'body-parser';
+import express, { Application } from "express";
+import cors from "cors";
+import { Pool } from "pg";
+import { dbConfig } from "./config";
+import bodyParser from "body-parser";
 
 const pool = new Pool(dbConfig);
 
@@ -10,35 +10,31 @@ const app: Application = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (_req, res) => {
+app.get("/", (_req, res) => {
     res.send("Server is running");
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
     try {
         const handle = req.body.handle;
         const password = req.body.password;
-        let query: string = `SELECT * FROM users WHERE handle = '${handle}'`;
-        const results = await pool.query(query);
+        let query: string = `SELECT * FROM users WHERE handle = $1 AND password = $2`;
+        const results = await pool.query(query, [handle, password]);
 
         if (results.rowCount != 0) {
-            const user = results.rows[0]
-            if (user.password == password) {
-                res.status(203).json(user);
-            } else {
-                res.status(403).send("Password incorrect");
-            }
+            const user = results.rows[0];
+            res.status(203).json(user);
         } else {
-            res.status(403).send("User not found");
+            res.status(403).send("Wrong handle or password");
         }
     } catch (error) {
         res.status(403).send("Something went wrong..");
     }
 });
 
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
     try {
         const handle = req.body.handle;
         const firstname = req.body.firstname;
@@ -46,13 +42,22 @@ app.post('/signup', async (req, res) => {
         const email = req.body.email;
         const password = req.body.password;
         let query: string = `INSERT INTO users(handle, firstname, lastname,
-                                               email, password) VALUES
-        ('${handle}', '${firstname}', '${lastname}', '${email}',
-         '${password}')`;
-        await pool.query(query);
+            email, password) VALUES ($1, $2, $3, $4, $5)`;
+        await pool.query(query, [handle, firstname, lastname, email, password]);
         res.status(200).send("User has been registered");
     } catch (error) {
         res.status(403).send("Something went wrong..");
+    }
+});
+
+app.get("/users", async (_req, res) => {
+    try {
+        let query: string = 'SELECT * FROM users';
+        const results = await pool.query(query);
+        res.json(results.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(403).send("Something went wrong");
     }
 });
 
