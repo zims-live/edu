7288b058e313613176fcaa7cda8ba95626f7ce1f
yzia2000 <e0381439@u.nus.edu>
2021-01-23 13:00:50 +0530

    Fixed testing issues

--- core/api/auth/src/app.spec.ts
+++ core/api/auth/src/app.spec.ts
@@ -1,57 +1,78 @@
-const request = require('supertest');
+import request from 'supertest';
 import app from './app';
 
 describe('Auth route test', () => {
-  it('signup', async done => {
+  it('signup', (done) => {
     request(app)
-    .post('/auth/signup')
-    .send({
-      handle: 'susan',
-      firstname: 'Susan',
-      lastname: 'Parker',
-      password: 'susan',
-      email: 'susan@test.com',
-    })
-    .expect(200, done)
+      .post('/auth/signup')
+      .send({
+        handle: 'susan',
+        firstname: 'Susan',
+        lastname: 'Parker',
+        password: 'susan',
+        email: 'susan@test.com'
+      })
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        done(err);
+      });
   });
 
-  it('login', async done => {
+  it('login', (done) => {
     request(app)
-    .post('/auth/login')
-    .send({
-      handle: 'susan',
-      password: 'susan',
-    })
-    .expect(203, done)
+      .post('/auth/login')
+      .send({
+        handle: 'susan',
+        password: 'susan'
+      })
+      .expect(203)
+      .end((err) => {
+        if (err) return done(err);
+        done(err);
+      });
   });
 
-  it('login handle fail', async done => {
+  it('login handle fail', (done) => {
     request(app)
-    .post('/auth/login')
-    .send({
-      handle: 'Susan',
-      password: 'susan',
-    })
-    .expect(403, done)
+      .post('/auth/login')
+      .send({
+        handle: 'Susan',
+        password: 'susan'
+      })
+      .expect(403)
+      .end((err) => {
+        if (err) return done(err);
+        done(err);
+      });
   });
 
-  it('login password fail', async done => {
+  it('login password fail', (done) => {
     request(app)
-    .post('/auth/login')
-    .send({
-      handle: 'susan',
-      password: 'Susan',
-    })
-    .expect(403, done)
+      .post('/auth/login')
+      .send({
+        handle: 'susan',
+        password: 'Susan'
+      })
+      .expect(403)
+      .end((err) => {
+        if (err) return done(err);
+        done(err);
+      });
   });
 
-  it('login both fail', async done => {
+  it('login both fail', (done) => {
     request(app)
-    .post('/auth/login')
-    .send({
-      handle: 'Susan',
-      password: 'Susan',
-    })
-    .expect(403, done)
+      .post('/auth/login')
+      .send({
+        handle: 'Susan',
+        password: 'Susan'
+      })
+      .expect(403)
+      .expect(403)
+      .end((err) => {
+        if (err) return done(err);
+        done(err);
+      });
   });
 });
