ab370a21ade65573a6b4a44daa60db586b6b5aae
yzia2000 <e0381439@u.nus.edu>
2021-02-06 22:38:36 +0530

    added user foreign keys to lms Enrolls and Teaches relations

--- core/api/auth/dbscripts/1_users_table.sql
+++ core/api/auth/dbscripts/1_users_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users(userid SERIAL PRIMARY KEY, handle varchar(255) UNIQUE, firstname text, lastname text, email text UNIQUE, password text);
+CREATE TABLE Users(id SERIAL PRIMARY KEY, handle varchar(255) UNIQUE, firstname text, lastname text, email text UNIQUE, password text);
